3b1de95f528bcdd037095db12ea35ca9eed640fe
Sohaib Mohiuddin <43653130+Sohaib-Mohiuddin@users.noreply.github.com>
2019-11-29 22:09:33 -0500

    Update login.php

--- data-project/login.php
+++ data-project/login.php
@@ -19,26 +19,12 @@
     </div>
     <form method='POST' action='login.php'>
 
-        <!-- <div class="input-group col-lg-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1">ID:</span>
-            </div>
-            <input type="number" name='studentid' autocomplete="off" max = "99999999" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
-        </div> -->
-
         <div class="input-group col-lg-3">
             <span class="input-group-addon" id="basic-addon1">ID: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
             <input type="number" name='studentid' autocomplete="off" max = "99999999" class="form-control" aria-describedby="basic-addon1">
         </div>
 
         <br>
-
-        <!-- <div class="input-group mb-3 col-lg-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon1">Password:</span>
-            </div>
-            <input type='password' name='password' autocomplete="off" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
-        </div> -->
 
         <div class="input-group col-lg-3">
             <span class="input-group-addon" id="basic-addon1">Password:</span>
@@ -53,16 +39,6 @@
             <option value="professor">Professor</option>
             <option value="admin">Admin</option>
         </select>
-        
-        <!-- <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-            <span class="caret"></span></button>
-            <ul class="dropdown-menu" name="dropdown">
-                <li><a value="student">Student</a></li>
-                <li><a value="professor">Professor</a></li>
-                <li><a value="admin">Admin</a></li>
-            </ul>
-        </div> -->
 
         <br><br>
 
